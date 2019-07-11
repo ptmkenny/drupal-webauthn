8eee8de7c701227483dd1deb3328d93bb9d882b4
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2019-07-11 13:12:45 +0200

    Add base forms for register/login operations

---
+++ src/Form/PublicKeyCredentialCreationForm.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\webauthn\Form;
+
+use Drupal\user\AccountForm;
+
+/**
+ * Implements a registration form based on PK creation.
+ */
+class PublicKeyCredentialCreationForm extends AccountForm {
+
+}
---
+++ src/Form/PublicKeyCredentialRequestForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\webauthn\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements login form based on PK validation.
+ */
+class PublicKeyCredentialRequestForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'pk_credential_request_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+}
