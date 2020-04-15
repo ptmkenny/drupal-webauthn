b8d7b7f0fee6349fce0116306d79733272b68ff7
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2020-04-15 19:46:51 +0200

    Add field-type to store trust path information
    Signed-off-by: Dennis A. Torres Rodriguez <d70rr3s@gmail.com>

---
+++ src/Plugin/Field/FieldType/TrustPathItem.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\webauthn\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\Plugin\Field\FieldType\MapItem;
+use Webauthn\TrustPath\EmptyTrustPath;
+use Webauthn\TrustPath\TrustPath;
+
+/**
+ * Field-type implementation to store trust-path value for public keys.
+ *
+ * @see \Webauthn\TrustPath\TrustPath
+ *
+ * @FieldType(
+ *   id = "trust_path",
+ *   label = @Translation("Trust path"),
+ *   description = @Translation("Stores trust path information"),
+ *   no_ui = "true",
+ *   cardinality = 1
+ * )
+ */
+class TrustPathItem extends MapItem {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
+    return (new EmptyTrustPath())->jsonSerialize();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setValue($values, $notify = TRUE) {
+    if ($values instanceof TrustPath) {
+      $values = $values->jsonSerialize();
+    }
+
+    parent::setValue($values, $notify);
+  }
+
+}
